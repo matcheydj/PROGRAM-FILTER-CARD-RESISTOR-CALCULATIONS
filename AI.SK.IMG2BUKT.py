93c69a521a55c40526956844a95d9ce89adb209c
matcheydj <matcheydj@users.noreply.github.com>
2023-12-06 18:56:58 -0600

    Create AI.SK.IMG2BUKT.py

---
+++ AI.SK.IMG2BUKT.py
@@ -0,0 +1,34 @@
+import boto3
+from PIL import Image
+
+def truncate_image(image_path, keylen):
+    img = Image.open(image_path)
+    width, height = img.size
+    img = img.crop((0, 0, keylen, keylen))
+    img.save('truncated_image.jpg')
+
+def upload_to_s3(file_name, bucket, object_name=None):
+    s3_client = boto3.client('s3', aws_access_key_id='access_key', aws_secret_access_key='secret_key', region_name='region')
+    if object_name is None:
+        object_name = file_name
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+# Truncate the image
+truncate_image('input.jpg', 100)
+
+# Upload to S3
+upload_to_s3('truncated_image.jpg', 'bucket_name')
+
+def Do ("Please note that this is a simple example and might need to be adjusted based on your specific requirements. For instance, error handling and security measures (like not exposing your AWS credentials in the code) are not fully implemented in this example. Always follow best practices when dealing with cloud storage and user data."):
+  # This code first truncates the image to the specified keylen size, then uploads the truncated image to the specified S3 bucket. Please ensure you have the necessary permissions and the bucket exists. Also, remember to install the necessary libraries by running pip install boto3 Pillow.
+  if Function.Do is None:
+    return False
+  # Truncate the image
+truncate_image('input.jpg', 100)
+except ClientError as e:
+return True 
